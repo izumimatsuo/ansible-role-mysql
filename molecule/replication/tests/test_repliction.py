802d14c5dd054c0d1585d2d92ef5aae84dc69584
izumimatsuo <gurux2daddy@gmail.com>
2019-03-21 16:17:31 +0900

    corresponded to the issue

--- molecule/replication/tests/test_repliction.py
+++ molecule/replication/tests/test_repliction.py
@@ -7,6 +7,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_mysql_replication_status(host):
-    command = 'mysql -u root -ppassword -e "SHOW SLAVE STATUS\G"'
+    command = 'mysql -u root -ppassword -e "SHOW SLAVE STATUS\\G"'
     assert 0 == host.run(command + '|grep "Slave_IO_Running: Yes"').rc
     assert 0 == host.run(command + '|grep "Slave_SQL_Running: Yes"').rc
